51c44480730143d981b15ec54e8cbe5687b3ef7c
Aaro Saila <aaros.saila@gmail.com>
2023-12-20 14:22:48 +0200

    -

---
+++ 2023/day02/day02-02.py
@@ -0,0 +1,95 @@
+import re
+
+class txtFile:
+    def __init__(self, path):
+        self.path = path
+        self.contents = open(path, "r").read().splitlines()
+
+
+class Game:
+    games = {}
+    sum_of_powers = 0
+    def __init__(self, id):
+        self.id = id
+        self.sets = []
+        self.minimums = {"reds": None, "greens": None, "blues": None}
+        self.power = None
+        if self.id != None:
+            self.games["Game " + str(self.id)] = self
+
+    def check_minimums(self):
+        reds = []
+        greens = []
+        blues = []
+        for _set in self.sets:
+            reds.append(_set.reds)
+            greens.append(_set.greens)
+            blues.append(_set.blues)
+        reds.sort()
+        greens.sort()
+        blues.sort()
+        self.minimums["reds"] = reds[-1]
+        self.minimums["greens"] = greens[-1]
+        self.minimums["blues"] = blues[-1]
+        return
+    
+    def check_power(self):
+        self.power = self.minimums["reds"] * self.minimums["greens"] * self.minimums["blues"]
+        Game.sum_of_powers += self.power
+        return        
+
+
+class Set:
+    def __init__(self, cubes):
+        self.cubes = cubes.split(",")
+        self.reds = color_count("red", self.cubes)
+        self.greens = color_count("green", self.cubes)
+        self.blues = color_count("blue", self.cubes)
+
+
+def make_dict(list):
+    games_dict = {}
+    x = 1
+    for game in list:
+        game_list = game.split(":")
+        games_dict[x] = game_list[1].split(";")
+        x += 1
+    
+    return games_dict
+
+
+def color_count(color, cubes):
+    color_count = 0
+    for cube in cubes:
+            if color in cube:
+                nums = re.findall("\d", cube)
+                try:
+                    nums[1]
+                    nums_string = ""
+                    for num in nums:
+                        nums_string += "".join(num)
+                    nums = int(nums_string)
+                    color_count += nums
+                except(IndexError):
+                    color_count += int(nums[0])
+    return color_count
+
+
+input_data = txtFile("2023/day02/input.txt").contents
+
+input_dict = make_dict(input_data)  # example: 1: [' 3 blue, 4 red', ' 1 red, 2 green, 6 blue', ' 2 green']
+
+for i in range(1, len(input_dict) + 1):
+    game = Game(i)
+    for _set in input_dict[i]:
+        game.sets.append(Set(_set))
+    game.check_minimums()
+    game.check_power()
+
+for i in range(1, len(Game.games) + 1):
+    print(Game.games["Game " + str(i)].minimums)
+    print(Game.games["Game " + str(i)].power)
+
+print("--------------------------------------------")
+print(Game.sum_of_powers)
+print("--------------------------------------------")
---
+++ 2023/day03/day03-01.py
@@ -0,0 +1,102 @@
+import re
+
+class txtFile:
+    def __init__(self, path):
+        self.path = path
+        self.contents = open(path, "r").read().splitlines()
+
+
+class Line:
+    lines = []  # line objects
+    def __init__(self, string, id):
+        def get_numbers():
+            numbers = re.findall("\d+", self.string)
+            for number in numbers:
+                number_object = Number(self, number)
+                self.numbers.append(number_object)
+            return
+    
+
+        self.id = id
+        self.string = string
+        self.numbers = []  # Number objects
+        get_numbers()
+        Line.lines.append(self)
+
+
+class Number:
+    numbers = [] # Number objects
+    def __init__(self, line, value):
+        def get_span():
+            iters = re.finditer("\d+", self.line.string)
+            for i in iters:
+                if i.group() == self.value:
+                    return (i.start(), i.end())
+                
+        
+        self.line = line  # line object
+        self.value = value  # number
+        self.span = get_span()
+        self.part_number = False  # /True
+        Number.numbers.append(self)
+        
+    def check_if_part_number(self):
+        def check_strings():
+            # Sets the span to scan from string
+            if self.span[0] == 0:
+                span_to_scan = (self.span[0], self.span[1] + 1)
+            elif self.span[1] == len(self.line.string):
+                span_to_scan = (self.span[0] - 1, self.span[1])
+            else:
+                span_to_scan = (self.span[0] - 1, self.span[1] + 1)
+            # Tries to check line above
+            try:
+                string_above = Line.lines[self.line.id - 1].string
+                string_to_scan = string_above[span_to_scan[0] : span_to_scan[1]]
+                if re.search("[^\d.]", string_to_scan) != None:
+                    self.part_number = True
+                    return
+            except(IndexError):
+                pass
+            
+            # Checks own line
+            string_to_scan = self.line.string[span_to_scan[0] : span_to_scan[1]]
+            if re.search("[^\d.]", string_to_scan) != None:
+                self.part_number = True
+                return
+        
+            # Tries to check line below
+            try:
+                string_below = Line.lines[self.line.id + 1].string
+                string_to_scan = string_below[span_to_scan[0] : span_to_scan[1]]
+                if re.search("[^\d.]", string_to_scan) != None:
+                    self.part_number = True
+                    return
+            except(IndexError):
+                pass
+
+            return
+        
+
+        check_strings()
+
+        return
+
+
+def sum_part_numbers(numbers):
+    result = 0
+    for number in numbers:
+        if number.part_number == True:
+            result += int(number.value)
+    return result
+
+
+input_data = txtFile("2023/day03/input.txt").contents
+
+for line in input_data:
+    line = Line(line, input_data.index(line))
+
+for number in Number.numbers:
+    number.check_if_part_number()
+
+print(sum_part_numbers(Number.numbers))
