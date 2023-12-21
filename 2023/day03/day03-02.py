6103372b82c6d56f14b2fca6284c61a00891da10
Aaro Saila <aaros.saila@gmail.com>
2023-12-21 21:59:51 +0200

    -

--- 2023/day03/day03-02.py
+++ 2023/day03/day03-02.py
@@ -9,18 +9,35 @@ class txtFile:
 class Line:
     lines = []  # line objects
     def __init__(self, string, id):
-        def get_stars():
-            stars = re.finditer("\*", self.string)
+        def make_stars():
+            star_list = []
+            stars = re.finditer(r"\*", self.string)
             for star in stars:
-                star_object = Star(self, (star.start(), star.end()))
-                self.stars.append(star_object)
-            return
-    
+                star_span = (star.start(), star.end())
+                star_object = Star(self, star_span)
+                star_list.append(star_object)
+            
+            return star_list
+        
+        def make_numbers():
+            number_list = []
+            numbers = re.finditer(r"\d+", self.string)
+            for number in numbers:
+                number_value = int(number.group())
+                if number.start() == 0:
+                    number_span = range(number.start(), number.end() + 1)
+                else:
+                    number_span = range(number.start() - 1, number.end() + 1)
+                number_object = Number(self, number_value, number_span)
+                number_list.append(number_object)
 
+            return number_list
+
+    
         self.id = id
         self.string = string
-        self.stars = []  # Star objects
-        get_stars()
+        self.stars = make_stars()
+        self.numbers = make_numbers()
         Line.lines.append(self)
 
 
@@ -28,71 +45,70 @@ class Star:
     stars = []  # Star objects
     def __init__(self, line, span):
         self.line = line  # line object
-        self.span = span  # tuple
+        self.span = span  # range
         self.ratio_nums = []  # ints
         self.ratio = 1  # Changes with method
         Star.stars.append(self)
     
     def get_ratio(self):
-        # Sets the span to be scanned
-        if self.span[0] == 0:
-            span_to_scan = (self.span[0], self.span[1] + 1)
-        elif self.span[1] == len(self.line.string):
-            span_to_scan = (self.span[0] - 1, self.span[1])
-        else:
-            span_to_scan = (self.span[0] - 1, self.span[1] + 1)
+        def append_adjacent_numbers_of_line(line):
+            numbers = line.numbers
+            for number in numbers:
+                star_adjacent_to_number = (self.span[0] or self.span[1]) in number.span
+                if star_adjacent_to_number:
+                    self.ratio_nums.append(number.value)
+            return
+
+
+        def calculate_ratio():
+            star_has_exactly_two_adjacent_nums = len(self.ratio_nums) == 2
+            if star_has_exactly_two_adjacent_nums:
+                for number in self.ratio_nums:
+                    self.ratio *= number
+            return
+
         # Tries to check line above
         try:
-            string_above = Line.lines[self.line.id - 1].string
-            string_to_scan = string_above[span_to_scan[0] : span_to_scan[1]]
-            scan = re.search("\d+", string_to_scan)
-            if scan != None:
-                self.ratio_nums.append(int(scan.group()))
+            line_above = Line.lines[self.line.id - 1]
+            append_adjacent_numbers_of_line(line_above)
+            
         except(IndexError):
             pass
-        
+
         # Checks own line
-        string_to_scan = self.line.string[span_to_scan[0] : span_to_scan[1]]
-        scan = re.search("\d+]", string_to_scan)
-        if scan != None:
-            self.ratio_nums.append(int(scan.group()))
-            return
-    
+        own_line = self.line
+        append_adjacent_numbers_of_line(own_line)
+        
         # Tries to check line below
         try:
-            string_below = Line.lines[self.line.id + 1].string
-            string_to_scan = string_below[span_to_scan[0] : span_to_scan[1]]
-            scan = re.search("\d+", string_to_scan)
-            if scan != None:
-                self.ratio_nums.append(int(scan.group()))
+            line_below = Line.lines[self.line.id + 1]
+            append_adjacent_numbers_of_line(line_below)
+
         except(IndexError):
             pass
 
-        for num in self.ratio_nums:
-            self.ratio *= num
-        
+        calculate_ratio()
+
         return
 
+class Number:
+    numbers = [] # Number objects
+    def __init__(self, line, value, span):
+        self.line = line  # line object
+        self.value = value  # int
+        self.span = span  # range
+        Number.numbers.append(self)
 
-def sum_of_ratios(stars):
+
+def sum_of_ratios():
     sum = 0
-    for star in stars:
-        sum += star.ratio
+    for star in Star.stars:
+        if star.ratio != 1:
+            sum += star.ratio
     return sum
 
 
-input_data = [
-    "467..114..",
-    "...*......",
-    "..35..633.",
-    "......#...",
-    "617*......",
-    ".....+.58.",
-    "..592....."
-    "......755.",
-    "...$.*....",
-    ".664.598.."
-]
+input_data = txtFile("2023/day03/input.txt").contents
 
 for line in input_data:
     line_object = Line(line, input_data.index(line))
@@ -100,4 +116,4 @@ for line in input_data:
 for star in Star.stars:
     star.get_ratio()
 
-print(sum_of_ratios(Star.stars))
+print(sum_of_ratios())
--- 2023/day03/test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-a = "...*...*.....*"
-
-b = re.finditer("\*", a)
-
-for i in b:
-    print(i.start(), i.end())
