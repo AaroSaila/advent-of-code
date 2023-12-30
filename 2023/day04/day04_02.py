b39b26451cedfed65c07156971db919e29e31ceb
Aaro Saila <aaros.saila@gmail.com>
2023-12-30 16:52:45 +0200

    -

--- 2023/day04/day04_02.py
+++ 2023/day04/day04_02.py
@@ -10,12 +10,33 @@ class TxtFile:
 
 class Card:
     cards = []  # Card objects
+    total_cards = None
 
-    def __init__(self, index, winning_numbers, numbers):
-        self.index = index
-        self.winning_numbers = winning_numbers
-        self.numbers = numbers
+    def __init__(self, winning_numbers, numbers):
+        self.winning_numbers = winning_numbers  # set
+        self.numbers = numbers  # set
+        self.iterations = 1
         Card.cards.append(self)
+
+    def update_iterations(self):
+        index = Card.cards.index(self)
+        hits = 0
+        for number in self.winning_numbers:
+            if number in self.numbers:
+                hits += 1
+                card_to_be_changed = Card.cards[index + hits]
+                card_to_be_changed.iterations += 1
+
+    def check_card(self):
+        for i in range(self.iterations):
+            self.update_iterations()
+        return
+
+    def count_cards(self):
+        Card.total_cards = 0
+        for card in Card.cards:
+            Card.total_cards += card.iterations
+        return
 
 
 def get_numbers_for_card(separated_card_string):
@@ -25,6 +46,16 @@ def get_numbers_for_card(separated_card_string):
     return numbers
 
 
-input_data = TxtFile("2023/day04/example.txt").contents
+input_data = TxtFile("2023/day04/input.txt").contents
 
-print(input_data)
+for card in input_data:
+    numbers_list = card.split(":").pop().split("|")
+    winning_numbers = get_numbers_for_card(numbers_list[0])
+    numbers = get_numbers_for_card(numbers_list[1])
+    Card(winning_numbers, numbers)
+
+for card in Card.cards:
+    card.check_card()
+
+Card.count_cards(Card)
+print(Card.total_cards)
--- 2023/day04/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-a = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-
-b = a.split(":").pop().split("|")
-
-print(b)
