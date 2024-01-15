c927f56789552c5906f194b93e6285e1776d2787
Aaro Saila <aaros.saila@gmail.com>
2024-01-15 12:47:31 +0200

    day05

--- 2023/day05/day05_01.py
+++ 2023/day05/day05_01.py
@@ -1,87 +1,108 @@
-class txtFile:
-    def __init__(self, path):
-        self.path = path
-        with open(self.path, "r") as file:
-            self.contents = file.read().splitlines()
+"""advent of code day 5 part 1 version 2"""
+import re
 
 
-class Map:
-    maps = []  # Map objects
+def process_input_data(data):
+    """Processes the input data"""
 
-    def __init__(self, row_data):
-        def process_row_data(row_data):
-            rows = []
-            for row in row_data:
-                rows.append(Row(row))
-            return rows
+    def get_seeds(data):
+        local_seeds = data[0].split(":")[1].split(" ")
+        local_seeds.remove("")
+        seeds_list = []
+        for seed in local_seeds:
+            seeds_list.append(int(seed))
+        local_seeds = seeds_list
+        return local_seeds
 
-        self.rows = process_row_data(row_data)  # Row objects
-        Map.maps.append(self)
+    def get_next_map(data):
+        def process_row(row_string):
+            row = {}
+            row_numbers = []
+            row_numbers_string = row_string.split(" ")
+            for number in row_numbers_string:
+                row_numbers.append(int(number))
+            row["source"] = row_numbers[1]
+            row["destination"] = row_numbers[0]
+            row["range"] = row_numbers[2]
+            return row
 
-    def source_to_destination(self, seed):
-        for row in self.rows:
-            source_start = row.source_range_start
-            source_end = source_start + row.range_length
-            destination_start = row.destination_range_start
-            if seed < source_start or seed > source_end:
-                pass
+        name = data[0].replace(" map:", "")
+        data.pop(0)
+        rows = []
+        to_be_removed = []
+        for item in data:
+            if re.search(r"\d", item) is not None:
+                rows.append(process_row(item))
+                to_be_removed.append(item)
             else:
-                seed = destination_start + (seed - source_start)
-        return seed
+                for item2 in to_be_removed:
+                    data.remove(item2)
+                    to_be_removed = []
+                break
+        if to_be_removed:
+            for item2 in to_be_removed:
+                data.remove(item2)
+        return {"name": name, "rows": rows}
 
+    processed_data = {}
 
-class Row:
-    def __init__(self, row_list_ints):
-        self.destination_range_start = row_list_ints[0]
-        self.source_range_start = row_list_ints[1]
-        self.range_length = row_list_ints[2]
+    while "" in data:
+        data.remove("")
 
+    processed_data["seeds"] = get_seeds(data)
+    data.pop(0)
+    maps_remaining = True
+    while maps_remaining:
+        try:
+            _map = get_next_map(data)
+            processed_data[_map["name"]] = _map["rows"]
+        except IndexError:
+            maps_remaining = False
 
-def get_data_from_contents(contents):
-    for _string in contents:
-        contents[contents.index(_string)] = _string.split(" ")
-    for _list in contents:
-        for number_string in _list:
-            _list[_list.index(number_string)] = int(number_string)
-    return contents
+    return processed_data
 
+def process_seed(seed, seed_maps):
+    """Processes seed through given maps"""
 
-def get_data(path):
-    data = get_data_from_contents(txtFile(path).contents)
-    return data
+    def process_row(local_seed, source, destination, range_length):
+        if local_seed not in range(source, source + range_length):
+            return local_seed
+        difference = local_seed - source
+        result = destination + difference
+        return result
 
+    for _map in seed_maps.items():
+        for row in _map[1]:
+            original_seed = seed
+            seed = process_row(
+                seed,
+                row["source"],
+                row["destination"],
+                row["range"]
+            )
+            if seed != original_seed:
+                break
 
-def process_seeds(seeds):
-    for seed in seeds:
-        seeds[seeds.index(seed)] = int(seed)
-    return
+    return seed
 
+with open(
+    "/home/aaro/Documents/Code/advent-of-code/2023/day05/input.txt",
+    "r",
+    encoding="utf-8"
+) as file:
+    input_data = file.read().splitlines()
 
-seeds = txtFile("2023/day05/inputs/seeds.txt").contents[0].split(" ")
-process_seeds(seeds)
-seed_to_soil_data = get_data(
-    "2023/day05/inputs/01_seed_to_soil_map.txt"
-)
-soil_to_fertilizer_data = get_data(
-    "2023/day05/inputs/02_soil_to_fertilizer_map.txt"
-)
-fertilizer_to_water_data = get_data(
-    "2023/day05/inputs/03_fertilizer_to_water_map.txt"
-)
-water_to_light_data = get_data(
-    "2023/day05/inputs/04_water_to_light_map.txt"
-)
-light_to_temperature_data = get_data(
-    "2023/day05/inputs/05_light_to_temperature_map.txt"
-)
-temperature_to_humidity_data = get_data(
-    "2023/day05/inputs/06_temperature_to_humidity_map.txt"
-)
-humidity_to_location_data = get_data(
-    "2023/day05/inputs/07_humidity_to_location_map.txt"
-)
+input_data = process_input_data(input_data)
+seeds = input_data["seeds"]
+maps = {}
 
-print(seeds)
+for _map in input_data.items():
+    if _map[0] != "seeds":
+        maps[_map[0]] = _map[1]
 
-seed_to_soil_map = Map(seed_to_soil_data)
-print(seed_to_soil_map.source_to_destination(seeds[0]))
+locations = []
+for i in seeds:
+    locations.append(process_seed(i, maps))
+locations.sort()
+
+print(locations)
--- 2023/day05/test.py
+++
@@ -1,9 +0,0 @@
-a = ["one", "two", "three"]
-b = [1, 2, 3]
-
-c = dict()
-
-for i in range(len(a)):
-    c[a[i]] = b[i]
-
-print(c)
